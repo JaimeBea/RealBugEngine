12d4feef7974331a0c65ff45801c123248aebd50
Jaime Bea <jaimebeapz@gmail.com>
2020-10-26 20:20:54 +0100

    Initialize OpenGL

--- ModuleRender.cpp
+++ ModuleRender.cpp
@@ -3,6 +3,7 @@
 #include "ModuleRender.h"
 #include "ModuleWindow.h"
 #include "SDL.h"
+#include "GL/glew.h"
 
 ModuleRender::ModuleRender()
 {
@@ -18,14 +19,33 @@ bool ModuleRender::Init()
 {
 	LOG("Creating Renderer context");
 
-	//glewInit();
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4); // desired version
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_COMPATIBILITY);
 
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1); // we want a double buffer
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24); // we want to have a depth buffer with 24 bits
+	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8); // we want to have a stencil buffer with 8 bits
+
+	SDL_GL_CreateContext(App->window->window);
+
+	glewInit();
+
+	glEnable(GL_DEPTH_TEST); // Enable depth test
+	glEnable(GL_CULL_FACE); // Enable cull backward faces
+	glFrontFace(GL_CCW); // Front faces will be counter clockwise
 
 	return true;
 }
 
 update_status ModuleRender::PreUpdate()
 {
+	int screen_width, screen_height;
+	SDL_GetWindowSize(App->window->window, &screen_width, &screen_height);
+
+	glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
 	return UPDATE_CONTINUE;
 }
 
@@ -38,6 +58,8 @@ update_status ModuleRender::Update()
 
 update_status ModuleRender::PostUpdate()
 {
+	SDL_GL_SwapWindow(App->window->window);
+
 	return UPDATE_CONTINUE;
 }
 
