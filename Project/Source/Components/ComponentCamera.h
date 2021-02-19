a9198256d81f854baaf16c9f57c801e40c9faf11
Franc Porta Solsona <f_xeski94@hotmail.com>
2021-02-19 03:28:43 +0100

    Components - Not MeshRenderer

--- Project/Source/Components/ComponentCamera.h
+++ Project/Source/Components/ComponentCamera.h
@@ -6,20 +6,20 @@
 
 class ComponentCamera : public Component {
 public:
-	REGISTER_COMPONENT(ComponentCamera, ComponentType::CAMERA);
+	REGISTER_COMPONENT(ComponentCamera, ComponentType::CAMERA); // Refer to ComponentType for the Constructor
 
+	// ------- Core Functions ------ //
 	void DrawGizmos() override;
 	void OnTransformUpdate() override;
 	void OnEditorUpdate() override;
 	void Save(JsonValue jComponent) const override;
 	void Load(JsonValue jComponent) override;
 
-	Frustum BuildDefaultFrustum() const;
+	Frustum BuildDefaultFrustum() const; // Builds a frustum object, at origin, facing Z direction, with specified FOV.
 
 public:
-	Frustum frustum = BuildDefaultFrustum();
-
+	Frustum frustum = BuildDefaultFrustum(); // Frustum geometry of the camera
 private:
-	bool activeCamera = false;
-	bool cullingCamera = false;
+	bool activeCamera = false;	// Indicator if this is the active camera. The active camera is the POV the scene will be rendered from.
+	bool cullingCamera = false; // Indicator if this camera perfoms frustum culling. Meshes outside the frustum will not be rendered when set to true.
 };
--- Project/Source/Components/ComponentLight.h
+++ Project/Source/Components/ComponentLight.h
@@ -12,8 +12,9 @@ enum class LightType {
 
 class ComponentLight : public Component {
 public:
-	REGISTER_COMPONENT(ComponentLight, ComponentType::LIGHT);
+	REGISTER_COMPONENT(ComponentLight, ComponentType::LIGHT); // Refer to ComponentType for the Constructor
 
+	// ------- Core Functions ------ //
 	void DrawGizmos() override;
 	void OnTransformUpdate() override;
 	void OnEditorUpdate() override;
@@ -21,17 +22,16 @@ public:
 	void Load(JsonValue jComponent) override;
 
 public:
-	bool drawGizmos = true;
+	bool drawGizmos = true;						  // If true, the Gizmos will be drawn on DrawGizmos().
+	LightType lightType = LightType::DIRECTIONAL; // Defines the type of the light from one in the LightType enum.
+	float3 pos = {0.0f, 0.0f, 0.0f};			  // World coordinates from where the light is emitting. Corresponds to the GameObject Transform and will update on OnTransformUpdate().
+	float3 direction = {0.0f, 0.0f, 0.0f};		  // Direction the light emits to. Corresponds to the GameObject Rotation and will update on OnTransformUpdate().
+	float3 color = {1.0f, 1.0f, 1.0f};			  // RGB color that will be reflected in the objects.
 
-	LightType lightType = LightType::DIRECTIONAL;
-
-	float3 pos = {0.0f, 0.0f, 0.0f};
-	float3 direction = {0.0f, 0.0f, 0.0f};
-	float3 color = {1.0f, 1.0f, 1.0f};
+	// -------- Attenuation -------- //
+	// Inital default values to rise 100 meters.
 	float intensity = 1.0f;
-
-	// Init value to rise 100 meters.
-	float kc = 1.0f; //Keep in one to avoid having denominator less than 1
+	float kc = 1.0f;		//Keep in one to avoid having denominator less than 1
 	float kl = 0.045f;
 	float kq = 0.0075f;
 	float innerAngle = pi / 12;
