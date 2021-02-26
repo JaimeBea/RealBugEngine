8850fa5358b73611d25f366133a1122ec1ec5174
magalenyo <magalenyo7@gmail.com>
2021-02-26 18:19:36 +0100

    2D Transform Component Init

---
+++ Project/Source/Components/ComponentTransform2D.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "Component.h"
+#include "Math/float2.h"
+#include "Math/float3.h"
+
+class ComponentTransform2D : public Component {
+public:
+	REGISTER_COMPONENT(ComponentTransform2D, ComponentType::TRANSFORM);
+	void Update() override;
+	void OnEditorUpdate() override;
+	
+
+	void SetPosition(float3 position);
+	void SetSize(float2 size);
+	void SetRotation(float3 rotation);
+	void SetScale(float3 scale);
+	void SetAnchorX(float2 anchorX);
+	void SetAnchorY(float2 anchorY);
+
+private:
+	float3 position = float3::zero;			// The offset position
+	float2 pivot	= float2(0.5, 0.5);		// The position of the pivot
+	float2 size		= float2::zero;			// The size of the item
+
+	float3 rotation	= float3::zero;			// The rotation of the element
+	float3 scale	= float3::one;			// The scale of the element
+
+	float2 anchorX	= float2::zero;			// The Anchor of X axis. AnchorX.x -> Min position, AnchorX.y -> Max position
+	float2 anchorY	= float2::zero;			// The Anchor of Y axis. AnchorY.x -> Min position, AnchorY.y -> Max position
+};
