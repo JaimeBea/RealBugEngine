ad1d0bba850799e0a855123f24308713a8a6dff0
magalenyo <magalenyo7@gmail.com>
2021-03-28 23:25:08 +0200

    Comment removal

--- Project/Source/Components/ComponentCanvasRenderer.cpp
+++ Project/Source/Components/ComponentCanvasRenderer.cpp
@@ -11,19 +11,12 @@ void ComponentCanvasRenderer::Load(JsonValue jComponent) {
 }
 
 void ComponentCanvasRenderer::Render(GameObject* gameObject) {
-	//We should get UI components (image, text,etc) here and call their render functions
-
 	ComponentTransform2D* transform2D = gameObject->GetComponent<ComponentTransform2D>();
 
 	ComponentImage* componentImage = gameObject->GetComponent<ComponentImage>();
 	if (componentImage != nullptr) {
 		componentImage->Draw(transform2D);
 	}
-
-	/*ComponentText* componentText = gameObject->GetComponent<componentText>();
-	if (componentText != nullptr) {
-		componentText->Render();
-	}*/
 }
 
 void ComponentCanvasRenderer::DuplicateComponent(GameObject& owner) {
