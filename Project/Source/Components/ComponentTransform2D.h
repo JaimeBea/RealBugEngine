f472ff62fac6d9301f1e6402c779763d1a2afc80
magalenyo <magalenyo7@gmail.com>
2021-03-31 23:54:11 +0200

    Documentation

--- Project/Source/Components/ComponentTransform2D.h
+++ Project/Source/Components/ComponentTransform2D.h
@@ -6,34 +6,33 @@
 #include "Math/float4x4.h"
 #include "Math/Quat.h"
 
+
+// Transform in the 2D Space. Used for UI Components.
 class ComponentTransform2D : public Component {
 public:
 	REGISTER_COMPONENT(ComponentTransform2D, ComponentType::TRANSFORM2D, false);
-	void Update() override;
-	void OnEditorUpdate() override;
-	void Save(JsonValue jComponent) const override;
-	void Load(JsonValue jComponent) override;
-	
+	void Update() override;								// Calculates the Global Matrix
+	void OnEditorUpdate() override;						// Works as input of the different values of the component
+	void Save(JsonValue jComponent) const override;		// Serializes Component
+	void Load(JsonValue jComponent) override;			// Deserializes Component
 
-	void SetPosition(float3 position);
-	void SetSize(float2 size);
-	void SetRotation(Quat rotation);
-	void SetRotation(float3 rotation);
-	void SetScale(float3 scale);
-	void SetAnchorX(float2 anchorX);
-	void SetAnchorY(float2 anchorY);
-	const float4x4 GetGlobalMatrix();
-	const float4x4 GetGlobalMatrixWithSize();
+	void SetPosition(float3 position);					// Sets this position to value
+	void SetSize(float2 size);							// Sets this size to value
+	void SetRotation(Quat rotation);					// Sets this rotation to value and calculates Euler Angles rotation
+	void SetRotation(float3 rotation);					// Sets this eulerAngles to value and calculates Quat rotation
+	void SetScale(float3 scale);						// Sets this scale to value
+	void SetAnchorX(float2 anchorX);					// Sets this anchorX to value
+	void SetAnchorY(float2 anchorY);					// Sets this anchorY to value
+	const float4x4 GetGlobalMatrix();					// Returns GlobalMatrix
+	const float4x4 GetGlobalMatrixWithSize();			// Returns GlobalMatrix with the size of the item
 
-	float3 GetPosition() const;
-	float2 GetSize() const;
-	float3 GetScale() const;
+	float3 GetPosition() const;							// Returns the position
+	float2 GetSize() const;								// Returns the size
+	float3 GetScale() const;							// Returns the scale
 
-	void InvalidateHierarchy();
-	void Invalidate();
-	void DuplicateComponent(GameObject& owner) override;
-
-
+	void InvalidateHierarchy();							// Invalidates hierarchy
+	void Invalidate();									// Invalidates component
+	void DuplicateComponent(GameObject& owner) override;	// Duplicates component (THIS SHOULDN'T BE USED)
 
 private:
 	float2 pivot	= float2(0.5, 0.5);		// The position of the pivot
@@ -44,13 +43,13 @@ private:
 	float3 localEulerAngles = float3::zero;	// The rotation of the element in Euler
 	float3 scale	= float3::one;			// The scale of the element
 
-	float2 anchorX	= float2::zero;			// The Anchor of X axis. AnchorX.x -> Min position, AnchorX.y -> Max position
-	float2 anchorY	= float2::zero;			// The Anchor of Y axis. AnchorY.x -> Min position, AnchorY.y -> Max position
+	float2 anchorX	= float2::zero;			// The Anchor of X axis. AnchorX.x -> Min position, AnchorX.y -> Max position. Currently not used.
+	float2 anchorY	= float2::zero;			// The Anchor of Y axis. AnchorY.x -> Min position, AnchorY.y -> Max position. Currently not used.
 
-	float4x4 localMatrix = float4x4::identity;
-	float4x4 globalMatrix = float4x4::identity;
+	float4x4 localMatrix = float4x4::identity;		// Local matrix
+	float4x4 globalMatrix = float4x4::identity;		// Global Matrix
 
 	bool dirty = true;
-
-	void CalculateGlobalMatrix();
+	
+	void CalculateGlobalMatrix();			// Calculates the Global Matrix
 };
--- Project/Source/Components/UI/ComponentButton.h
+++ Project/Source/Components/UI/ComponentButton.h
@@ -5,31 +5,33 @@
 #include "Math/float4.h"
 
 class GameObject;
-class ComponentButton : public ComponentSelectable //Class that allows user interaction by clicking/pressing
+
+// Component UI Button that allows user interaction by clicking/pressing the button. Inherits ComponentSelectable and implements IMouseClickHandler.
+class ComponentButton : public ComponentSelectable 
 					  , public IMouseClickHandler {
 public:
 	REGISTER_COMPONENT_SELECTABLE(ComponentButton, ComponentType::BUTTON, false);
-	void Init() override;
-	void OnEditorUpdate() override;
+	void Init() override;											// Inits the component
+	void OnEditorUpdate() override;									// Works as input of the Colors and the Selectable interface
 	
-	void OnClicked() override;
-	void Update() override;
-	void DuplicateComponent(GameObject& obj) override;
-	void OnPointerEnter() override;
-	void OnPointerExit() override;
+	void OnClicked() override;										// Action to perform when the button is clicked
+	void Update() override;											// Updates clicked variable if the button is Left-clicked mouse
+	void DuplicateComponent(GameObject& obj) override;				// TODO
+	void OnPointerEnter() override;									// Action to perform when the pointer enters
+	void OnPointerExit() override;									// Action to perform when the pointer exits
 
-	bool IsClicked() const;
-	void SetClicked(bool b);
+	bool IsClicked() const;											// Returns true if the button is clicked
+	void SetClicked(bool b);										// Sets clicked to the value
 
-	const float4 GetHoverColor() const;
-	const float4 GetClickColor() const;
-	const float4 GetDisableColor() const;
-	const float4 GetTintColor() const;
+	const float4 GetHoverColor() const;								// Returns colorHovered
+	const float4 GetClickColor() const;								// Returns colorClicked
+	const float4 GetDisableColor() const;							// Returns colorDisabled
+	const float4 GetTintColor() const;								// Returns the correspondant color of the current state
 
 private:
-	bool clicked = false;
+	bool clicked = false;											// Clicked state
 
-	float4 colorHovered = float4(0.84, 0.84, 0.84, 1);
-	float4 colorClicked = float4(0.64, 0.64, 0.64, 1);
-	float4 colorDisabled = float4(0.73, 0.73, 0.73, 1);
+	float4 colorHovered = float4(0.84, 0.84, 0.84, 1);				// The color when the button is hovered
+	float4 colorClicked = float4(0.64, 0.64, 0.64, 1);				// The color when the button is clicked
+	float4 colorDisabled = float4(0.73, 0.73, 0.73, 1);				// The color when the button is disabled
 };
