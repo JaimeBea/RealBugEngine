5f626e58ba8a8188d46bfe0de9af74f9a4591840
telmiuse <61976005+telmiuse@users.noreply.github.com>
2021-04-11 21:00:37 +0200

    Able and Disable UI with scripts

--- Game/Assets/Scripts/WinLose.cpp
+++ Game/Assets/Scripts/WinLose.cpp
@@ -8,18 +8,31 @@ GENERATE_BODY_IMPL(WinLose);
 
 void WinLose::Start() {
 	gameObject = GameplaySystems::GetGameObject("Win");
-	player = GameplaySystems::GetGameObject("Hearse");
-	UI = GameplaySystems::GetGameObject("InfoText");
+	player = GameplaySystems::GetGameObject("Fang");
+	loseText = GameplaySystems::GetGameObject("LoseText");
+	winText = GameplaySystems::GetGameObject("WinText");
 }
 
 void WinLose::Update() {
+	if (gameObject == nullptr) return;
+	if (player == nullptr) return;
+	if (loseText == nullptr) return;
+	if (winText == nullptr) return;
+
 	if (player->GetComponent<ComponentTransform>()->GetPosition().x >= WinPointX) {
-		player->GetComponent<ComponentTransform>()->SetPosition(float3(0.0f, 0.0f, 0.0f));
-		UI->Enable();
-		SceneManager::SceneLoad("Assets/Scenes/LoseScene.scene");
+		//player->GetComponent<ComponentTransform>()->SetPosition(float3(0.0f, 0.0f, 0.0f));
+		winText->Enable();
+		//SceneManager::SceneLoad("Assets/Scenes/LoseScene.scene");
+	}
+	else {
+		winText->Disable();
 	}
 	if (player->GetComponent<ComponentTransform>()->GetPosition().x <= LosePointX) {
-		player->GetComponent<ComponentTransform>()->SetPosition(float3(0.0f, 0.0f, 0.0f));
-		SceneManager::SceneLoad("Assets/Scenes/WinScene.scene");
+		//player->GetComponent<ComponentTransform>()->SetPosition(float3(0.0f, 0.0f, 0.0f));
+		//	SceneManager::SceneLoad("Assets/Scenes/WinScene.scene");
+		loseText->Enable();
+	}
+	else {
+		loseText->Disable();
 	}
 }
