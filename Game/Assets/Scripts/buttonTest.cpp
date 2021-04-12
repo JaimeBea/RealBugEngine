ea389af360718da6180183daf63ec5291e73fa48
telmiuse <61976005+telmiuse@users.noreply.github.com>
2021-04-12 22:56:19 +0200

    SceneStart

---
+++ Game/Assets/Scripts/buttonTest.cpp
@@ -0,0 +1,19 @@
+#include "buttonTest.h"
+
+#include "Utils/Logging.h"
+#include "GameObject.h"
+#include "GameplaySystems.h"
+
+GENERATE_BODY_IMPL(buttonTest);
+
+void buttonTest::Start() {
+	Button = GameplaySystems::GetGameObject("StartButton");
+}
+
+void buttonTest::Update() {
+}
+
+void buttonTest::OnButtonClick() {
+	/*SceneManager::SceneLoad("Assets/Scenes/LoseScene.scene");*/
+	LOG("Click");
+}
