2030c0b90e7b7740d69e9ae35bce2184df178c6a
telmiuse <61976005+telmiuse@users.noreply.github.com>
2021-04-13 16:09:45 +0200

    ButtonScript Clean

---
+++ Game/Assets/Scripts/TestPenteract.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "Script.h"
+
+class TestPenteract : public Script
+{
+	GENERATE_BODY(TestPenteract);
+
+public:
+
+	void Start() override;
+	void Update() override;
+
+};
+
