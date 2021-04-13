1e33b307802b648962e4ec80cc42974bae7c537a
FrancPS <58336040+FrancPS@users.noreply.github.com>
2021-04-13 21:51:25 +0200

    Added TreeFloating Script and metas. Remove .lib and .exe from Game

---
+++ Game/Assets/Scripts/TreeFloating.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Script.h"
+
+class GameObject;
+class ComponentTransform;
+class TreeFloating : public Script
+{
+	GENERATE_BODY(TreeFloating);
+
+public:
+
+	void Start() override;
+	void Update() override;
+
+public:
+	GameObject* gameObject = nullptr;
+
+private:
+	float offset = 0.f;
+	ComponentTransform* transform = nullptr;
+};
+
