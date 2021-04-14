b62231aca9dd4411386f8f4370bf8a0df0d3d4ba
jordi-gil <jordigilgonzalez@hotmail.com>
2021-04-14 15:24:16 +0200

    cleanning

--- Game/Assets/Scripts/CanvasImage.cpp
+++ Game/Assets/Scripts/CanvasImage.cpp
@@ -11,7 +11,12 @@ void CanvasImage::Start() {
 }
 
 void CanvasImage::Update() {
-	ComponentTransform2D* transform2D = gameObject->GetComponent<ComponentTransform2D>();
-	float2 newSize = float2(Screen::GetScreenWitdh(), Screen::GetScreenHeight());
-	transform2D->SetSize(newSize);
+	if (gameObject != nullptr) {
+		ComponentTransform2D* transform2D = gameObject->GetComponent<ComponentTransform2D>();
+		if (transform2D) {
+			float2 newSize = float2(Screen::GetScreenWitdh(), Screen::GetScreenHeight());
+			transform2D->SetSize(newSize);
+		}
+	}
+	
 }
