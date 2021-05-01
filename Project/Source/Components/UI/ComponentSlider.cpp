da2a9aecbd679ad2bfc00f2ed2984cee60372ae2
Guillem <guillemburguesmiro@gmail.com>
2021-05-01 10:41:05 +0200

    Basic component slider structure

---
+++ Project/Source/Components/UI/ComponentSlider.cpp
@@ -0,0 +1,28 @@
+#include "ComponentSlider.h"
+
+#include "Utils/Leaks.h"
+
+ComponentSlider::~ComponentSlider() {
+
+}
+
+void ComponentSlider::Init() {
+
+}
+
+void ComponentSlider::OnEditorUpdate() {
+
+}
+
+void ComponentSlider::Save(JsonValue jComponent) const {
+
+
+}
+
+void ComponentSlider::Load(JsonValue jComponent) {
+
+}
+
+void ComponentSlider::DuplicateComponent(GameObject& owner) {
+
+}
---
+++ Project/Source/Components/UI/ComponentSlider.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "Components/Component.h"
+#include "Components/UI/ComponentTransform2D.h"
+
+enum class DirectionType {
+	LEFT_TO_RIGHT,
+	RIGHT_TO_LEFT,
+	UP_TO_DOWN,
+	DOWN_TO_UP
+};
+
+class ComponentSlider : public Component {
+public:
+	REGISTER_COMPONENT(ComponentSlider, ComponentType::SLIDER, false);
+
+	~ComponentSlider();
+
+	void Init() override;										// Component Initialization
+	void OnEditorUpdate() override;								// Input for class variables
+
+	void Save(JsonValue jComponent) const override;				// Serializes the component
+	void Load(JsonValue jComponent) override;					// Deserializes the component
+	void DuplicateComponent(GameObject& owner) override;		// Duplicates the component
+
+private:
+	GameObject* background = nullptr;						
+	GameObject* fillArea = nullptr;
+	GameObject* handleArea = nullptr;
+
+	float maxValue = 100.0f;
+	float minValue = 0.0f;
+
+	float currentValue = 50.0f;									// Current value between maxValue and minValue
+	float normalizedValue = 0.5f;								// Normalized current value between 0 and 1
+
+	DirectionType direction = DirectionType::LEFT_TO_RIGHT;		// Slider direction
+};
