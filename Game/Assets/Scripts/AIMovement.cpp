417e22e2b262ecb74e45644f8cfd1337c5c2ba27
Isaac Fernandez Fontao <isaacfdez94@gmail.com>
2021-05-04 18:19:56 +0200

    Basic structure of the scritp

--- Game/Assets/Scripts/AIMovement.cpp
+++ Game/Assets/Scripts/AIMovement.cpp
@@ -1,5 +1,8 @@
 #include "AIMovement.h"
 
+#include "GameObject.h"
+#include "GameplaySystems.h"
+
 EXPOSE_MEMBERS(AIMovement) {
     // Add members here to expose them to the engine. Example:
     // MEMBER(MemberType::BOOL, exampleMember1),
@@ -10,9 +13,41 @@ EXPOSE_MEMBERS(AIMovement) {
 GENERATE_BODY_IMPL(AIMovement);
 
 void AIMovement::Start() {
-	
+    owner = GetOwner();
+    fang = GameplaySystems::GetGameObject("Fang");
+    //onimaru = GameplaySystems::GetGameObject("Onimaru");
+    Debug::Log("AI movement for %s Start Log", owner.name);
 }
 
 void AIMovement::Update() {
+    if (!owner.IsActive()) return;
+    if (!fang /* && !onimaru*/) return;
+
+    if (CharacterInSight(fang)) {
+        Seek(fang);
+    }
+    
+    /*else if (CharacterInSight(onimaru)) {
+        Seek(onimaru);
+    }*/
+
+    else {
+        //wander
+    }
+
+
+    
+
+
+
 	
+}
+
+bool AIMovement::CharacterInSight(GameObject* character)
+{
+    return false;
+}
+
+void AIMovement::Seek(GameObject* target)
+{
 }
--- Game/Assets/Scripts/AIMovement.h
+++ Game/Assets/Scripts/AIMovement.h
@@ -11,5 +11,20 @@ public:
 	void Start() override;
 	void Update() override;
 
+private:
+	bool CharacterInSight(GameObject* character);
+	void Seek(GameObject* target);
+
+public:
+
+	GameObject owner;
+	GameObject* fang = nullptr;
+	GameObject* onimaru = nullptr;
+
+private:
+
+	int speed = 8;
+	const float searchRadius = 50.f;
+
 };
 
