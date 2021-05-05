41552bc7b66f1d501877ba5ffff2cbfc9f2b5eba
Guillem <guillemburguesmiro@gmail.com>
2021-05-05 11:52:15 +0200

    WinLose script updated

--- Game/Assets/Scripts/WinLose.h
+++ Game/Assets/Scripts/WinLose.h
@@ -12,13 +12,19 @@ public:
 
 	void Start() override;
 	void Update() override;
-public:
 
+public:
+	UID winUID;
+	UID loseUID;
+	UID playerUID;
+
+	float LoseOffsetX = 2.0f;
+	float LoseOffsetZ = 2.0f;
+
+private:
 	GameObject* winCon = nullptr;
 	GameObject* loseCon = nullptr;
 	GameObject* player = nullptr;
 
-public:
-	float LoseOffsetX = 2.0f;
-	float LoseOffsetZ = 2.0f;
+
 };
