c650a414beb290ffda1ca8484beb486a50fcc034
FrancPS <58336040+FrancPS@users.noreply.github.com>
2021-05-06 17:49:06 +0200

    Included all libraries

--- Project/Source/Panels/PanelAbout.cpp
+++ Project/Source/Panels/PanelAbout.cpp
@@ -2,6 +2,7 @@
 
 #include "Application.h"
 #include "Modules/ModuleEditor.h"
+#include "Modules/ModuleHardwareInfo.h"
 
 #include "imgui.h"
 #include "IconsForkAwesome.h"
@@ -22,17 +23,24 @@ void PanelAbout::Update() {
 		ImGui::Text("Engine licensed under the MIT License.");
 		ImGui::Separator();
 
-		ImVec2 childSize = ImVec2(0, ImGui::GetTextLineHeightWithSpacing() * 6);
+		ImVec2 childSize = ImVec2(0, ImGui::GetTextLineHeightWithSpacing() * 8);
 
 		ImGui::Checkbox("Libraries", &showLibrary);
 		if (showLibrary) {
 			ImGui::BeginChildFrame(ImGui::GetID("cfg_infos"), childSize);
-			ImGui::Text("MathGeoLib v1.5 ");
-			ImGui::Text("Simple DirectMedia Layer (SDL) v2.0");
-			ImGui::Text("ImGui Docking");
-			ImGui::Text("OpenGL Extension Wrangler Library 2.1.0");
-			ImGui::Text("DevIL 1.8.0");
-			ImGui::Text("Assimp 1.4.1");
+			ImGui::Text("Simple DirectMedia Layer (SDL) v%s", App->hardware->sdlVersion);
+			ImGui::Text("OpenGL Extension Wrangler Library v%s", App->hardware->glewVersion);
+			ImGui::Text("Assimp v%s", App->hardware->assimpVersion);
+			ImGui::Text("Dev Image Library (DevIL) v%s", App->hardware->devilVersion);
+			ImGui::Text("MathGeoLib v%s", App->hardware->mathgeolibVersion);
+			ImGui::Text("ImGui Docking v%s", App->hardware->imguiVersion);
+			ImGui::Text("ImGuizmo v%s", App->hardware->imguizmoVersion);
+			ImGui::Text("FMT Lib v%s", App->hardware->fmtVersion);
+			ImGui::Text("FreeType v%s", App->hardware->freetypeVersion);
+			ImGui::Text("Libsndfile v%s", App->hardware->libsndVersion);
+			ImGui::Text("PhysicsFS v%s", App->hardware->physFSVersion);
+			ImGui::Text("RapidJSON v%s", App->hardware->rapidJsonVersion);
+			ImGui::Text("Open Audio Library (OpenAL) v%s", App->hardware->openALVersion);
 
 			ImGui::EndChildFrame();
 		}
