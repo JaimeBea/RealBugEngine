d52c74b44f4a15f9ceec5e36faf26772e86880b4
Devildrake <davidsierragonzalez@enti.cat>
2021-05-12 15:46:35 +0200

    Merge error + //TODO comment

--- Project/Source/GameplaySystems.cpp
+++ Project/Source/GameplaySystems.cpp
@@ -212,6 +212,7 @@ bool Screen::IsFullScreenOn() {
 }
 void Screen::SetFullScreen(bool fullscreen_) {
 	App->window->SetWindowMode(fullscreen_ ? WindowMode::FULLSCREEN : WindowMode::WINDOWED);
+}
 // --------- Camera --------- //
 
 bool Camera::CheckObjectInsideFrustum(GameObject* gameObject) {
